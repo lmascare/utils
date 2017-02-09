e639c5965082fa89a28a448fd99de7d9a2037723
Larry Mascarenhas <LMascare@kellynoah.com>
2017-02-09 15:36:12 -0500

    Rest Tutorial via Flask

---
+++ python/rest/rest2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#
+
+from flask import Flask, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading article ' + articleid
+
+@app.route('/hello')
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
+
+if __name__ == '__main__':
+    app.run()
