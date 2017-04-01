9851d946c345e1880346f29c54cdc725eb709f8d
Larry Mascarenhas <LMascare@lithium.kellynoah.com>
2017-04-01 15:11:16 -0400

    Django Store App

--- django/bookstore/bookstore/urls.py
+++ django/bookstore/bookstore/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
