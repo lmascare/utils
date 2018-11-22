6f7a8a5b8c2e12017f0bcdf6a7bb8ec4a54dcf60
Larry Mascarenhas <LMascare@lithium.kellynoah.com>
2018-11-21 20:45:57 -0500

    Multiprocessing and django tips

---
+++ python/admin/mp_q.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+from multiprocessing import Process, Queue
+
+def f(q):
+    q.put(['42', None, 'Hello'])
+
+if __name__ == "__main__":
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
