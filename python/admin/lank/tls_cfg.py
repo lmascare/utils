c5e000f450de2ffb92e973f58e0d5abe27fbf53d
Larry Mascarenhas <LMascare@lithium.kellynoah.com>
2019-02-11 21:30:58 -0500

    Multiprocessed nmap data collection and parsing

--- python/admin/lank/tls_cfg.py
+++ python/admin/lank/tls_cfg.py
@@ -7,14 +7,14 @@ host = os.uname()[1]
 
 hosts_to_scan = [
     "www.bloomberg.com",
-    "www.bloombergpolarlake.com"
+    "www.bloombergpolarlake.com",
     "www.google.com",
     "www.ibm.com",
     "www.incspot.com",
+    "www.oracle.com"
     "www.redhat.com",
     "www.americanprairie.org",
     "students.sbschools.org",
 ]
-ports_to_scan = ['443']
 
 recipient = ["larry.masc@gmail.com"]
